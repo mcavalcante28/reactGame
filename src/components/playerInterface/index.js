531a112c53c123e4920dcd996b527eec969d6a48
Marcus Cavalcante <mgc.cavalcante28@gmail.com>
2020-05-10 01:18:38 -0300

    Adicionando botões e estilização inicial

--- src/components/playerInterface/index.js
+++ src/components/playerInterface/index.js
@@ -3,7 +3,7 @@ import './styles.css'
 import rockImg from '../../assets/rock.svg'
 import paperImg from '../../assets/paper.svg'
 import scissorsImg from '../../assets/scissors.svg'
-import {FaRegHandScissors, FaHandRock, FaRegHandPaper} from 'react-icons/fa'
+import {FaRegHandScissors, FaRegHandRock, FaHandRock,FaRegHandPaper} from 'react-icons/fa'
 
 function playerInterface({playerName}) {
     // const name = this.props.playerName;
@@ -11,6 +11,17 @@ function playerInterface({playerName}) {
     <div className="component-container">
       <header>{playerName}</header>
       <img src={rockImg} style={{width: 120}}/>
+      <div className="options-container">
+        <button>
+          <FaRegHandRock size={25}/>
+        </button>
+        <button>
+          <FaRegHandScissors size={25}/>
+        </button>
+        <button>
+          <FaRegHandPaper size={25}/>
+        </button>
+      </div>
     </div>
   );
 }
